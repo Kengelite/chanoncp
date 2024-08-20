abdc73715ff3139a0bd93099825eb7deed0dbb58
Kengelite <apipath90@gmail.com>
2024-08-20 13:40:38 +0700

    20-08-2567 edit by keng

--- resources/views/person/personnel.blade.php
+++ resources/views/person/personnel.blade.php
@@ -25,17 +25,58 @@
   <link href="/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
   <!-- Template Main CSS File -->
   <link href="/assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+  <style>
+        table.dataTable thead th {
+            /* background-color: light;  */
+            /* color: black; */
+        }
+        .dataTables_wrapper .dataTables_filter {
+            float: left;
+            text-align: left;
+            position: relative;
+            margin-bottom:1rem;
+            /* top: -0.25rem;  */
+        }
+        .dataTables_wrapper .dataTables_length {
+            float: right;
+            text-align: right;
+        }
+
+        .pagination .page-item .page-link {
+            color: #007bff; /* สีตัวอักษร */
+            background-color: #fff; /* สีพื้นหลัง */
+            border: 1px solid #dee2e6; /* สีขอบ */
+        }
+
+        .pagination .page-item.active .page-link {
+            color: #fff;
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .pagination .page-item .page-link:hover {
+            color: #0056b3;
+            background-color: #e9ecef;
+            border-color: #dee2e6;
+        }
+        @media (max-width: 600px) {
+          .dataTables_wrapper .dataTables_filter {
+            float: left;
+            text-align: left;
+          }
+          .dataTables_wrapper .dataTables_length {
+              float: left;
+              text-align: left;
+          }
+        }
+    </style>
 </head>
 
 <body>
@@ -219,66 +260,6 @@
               </div>
             </div><!-- End Reports -->
 
-            <!-- Recent Sales -->
-            <div class="col-12">
-              <div class="card recent-sales overflow-auto">
-                <div class="card-body">
-                  <h5 class="card-title">Top personnel <span>| Today</span></h5>
-
-                  <table class="table table-borderless datatable">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Customer</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row"><a href="#">#2457</a></th>
-                        <td>Brandon Jacob</td>
-                        <td><a href="#" class="text-primary">At praesentium minu</a></td>
-                        <td>$64</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2147</a></th>
-                        <td>Bridie Kessler</td>
-                        <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
-                        <td>$47</td>
-                        <td><span class="badge bg-warning">Pending</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2049</a></th>
-                        <td>Ashleigh Langosh</td>
-                        <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
-                        <td>$147</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2644</a></th>
-                        <td>Angus Grady</td>
-                        <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
-                        <td>$67</td>
-                        <td><span class="badge bg-danger">Rejected</span></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#">#2644</a></th>
-                        <td>Raheem Lehner</td>
-                        <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
-                        <td>$165</td>
-                        <td><span class="badge bg-success">Approved</span></td>
-                      </tr>
-                    </tbody>
-                  </table>
-
-                </div>
-
-              </div>
-            </div><!-- End Recent Sales -->
-
             <!-- Top Selling -->
             <div class="col-12">
               <div class="card top-selling overflow-auto">
@@ -286,54 +267,92 @@
                
 
                 <div class="card-body pb-0">
-                  <h5 class="card-title">Top Selling <span>| Today</span></h5>
+                  <h5 class="card-title">บุคคลากร </h5>
 
-                  <table class="table table-borderless">
+                  <table class="table table-borderless display" id="dataperson" style="width:100%">
                     <thead>
                       <tr>
-                        <th scope="col">Preview</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Sold</th>
-                        <th scope="col">Revenue</th>
+                        <th scope="col">ภาพ</th>
+                        <th scope="col">ชื่อ</th>
+                        <th scope="col">จำนวนผลงาน</th>
+                        <th scope="col">ข้อมูล</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <th scope="row"><a href="#"><img src="/assets/img/product-1.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
-                        <td>$64</td>
-                        <td class="fw-bold">124</td>
-                        <td>$5,828</td>
+                      
+                        <td scope="row"><a href="#"><img src="/assets/img/product-1.jpg" alt=""></a></td>
+                        <td>นายอภิภัทร คำพุทธ</td>
+                        <td >124</td>
+                        <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="/assets/img/product-2.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
-                        <td>$46</td>
-                        <td class="fw-bold">98</td>
-                        <td>$4,508</td>
+                        <td scope="row"><a href="#"><img src="/assets/img/product-2.jpg" alt=""></a></td>
+                        <td>นายอภิภัทร คำพุทธ</td>
+                        <td >20</td>
+                        <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="/assets/img/product-3.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
-                        <td>$59</td>
-                        <td class="fw-bold">74</td>
-                        <td>$4,366</td>
+                        <td scope="row"><a href="#"><img src="/assets/img/product-3.jpg" alt=""></a></td>
+                        <td>นายอภิภัทร คำพุทธ</td>
+                        <td >30</td>
+                        <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
                       </tr>
                       <tr>
-                        <th scope="row"><a href="#"><img src="/assets/img/product-4.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
-                        <td>$32</td>
-                        <td class="fw-bold">63</td>
-                        <td>$2,016</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="/assets/img/product-5.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
-                        <td>$79</td>
-                        <td class="fw-bold">41</td>
-                        <td>$3,239</td>
-                      </tr>
+                      
+                      <td scope="row"><a href="#"><img src="/assets/img/product-1.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >124</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
+                    <tr>
+                      <td scope="row"><a href="#"><img src="/assets/img/product-2.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >20</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
+                    <tr>
+                      <td scope="row"><a href="#"><img src="/assets/img/product-3.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >30</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>  <tr>
+                      
+                      <td scope="row"><a href="#"><img src="/assets/img/product-1.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >124</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
+                    <tr>
+                      <td scope="row"><a href="#"><img src="/assets/img/product-2.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >20</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
+                    <tr>
+                      <td scope="row"><a href="#"><img src="/assets/img/product-3.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >30</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>  <tr>
+                      
+                      <td scope="row"><a href="#"><img src="/assets/img/product-1.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >124</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
+                    <tr>
+                      <td scope="row"><a href="#"><img src="/assets/img/product-2.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >20</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
+                    <tr>
+                      <td scope="row"><a href="#"><img src="/assets/img/product-3.jpg" alt=""></a></td>
+                      <td>นายอภิภัทร คำพุทธ</td>
+                      <td >30</td>
+                      <td>  <button class="btn btn-success"> ข้อมูล </button> </td>
+                    </tr>
                     </tbody>
                   </table>
 
@@ -489,62 +508,7 @@
             </div>
           </div><!-- End Budget Report -->
 
-         
-
-          <!-- News & Updates Traffic -->
-          <div class="card">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div>
-
-            <div class="card-body pb-0">
-              <h5 class="card-title">News &amp; Updates <span>| Today</span></h5>
-
-              <div class="news">
-                <div class="post-item clearfix">
-                  <img src="/assets/img/news-1.jpg" alt="">
-                  <h4><a href="#">Nihil blanditiis at in nihil autem</a></h4>
-                  <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
-                </div>
-
-                <div class="post-item clearfix">
-                  <img src="/assets/img/news-2.jpg" alt="">
-                  <h4><a href="#">Quidem autem et impedit</a></h4>
-                  <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...</p>
-                </div>
-
-                <div class="post-item clearfix">
-                  <img src="/assets/img/news-3.jpg" alt="">
-                  <h4><a href="#">Id quia et et ut maxime similique occaecati ut</a></h4>
-                  <p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...</p>
-                </div>
-
-                <div class="post-item clearfix">
-                  <img src="/assets/img/news-4.jpg" alt="">
-                  <h4><a href="#">Laborum corporis quo dara net para</a></h4>
-                  <p>Qui enim quia optio. Eligendi aut asperiores enim repellendusvel rerum cuder...</p>
-                </div>
-
-                <div class="post-item clearfix">
-                  <img src="/assets/img/news-5.jpg" alt="">
-                  <h4><a href="#">Et dolores corrupti quae illo quod dolor</a></h4>
-                  <p>Odit ut eveniet modi reiciendis. Atque cupiditate libero beatae dignissimos eius...</p>
-                </div>
-
-              </div><!-- End sidebar recent posts-->
-
-            </div>
-          </div><!-- End News & Updates -->
-
+        
         </div><!-- End Right side columns -->
 
       </div>
@@ -578,6 +542,21 @@
   <script src="/assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="/assets/vendor/php-email-form/validate.js"></script>
 
+  <script type="text/javascript">
+    $(document).ready(function() {
+        $('#dataperson').DataTable({
+            "order": [[2, 'desc']] ,
+            "pagingType": "simple_numbers", // ใช้การแบ่งหน้าแบบมีปุ่ม Previous และ Next
+            "language": {
+                "paginate": {
+                    "previous": "Previous",
+                    "next": "Next"
+                }
+            },
+            // "dom": '<"top"i>rt<"bottom"flp><"clear">'
+        });
+    });
+</script>
   <!-- Template Main JS File -->
   <!-- <script src="/assets/js/main.js"></script> -->
 
